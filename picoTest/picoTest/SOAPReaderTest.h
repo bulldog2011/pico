4aee999ddafe128c5073d8138f2c85c0c5ed7226
bulldog <51startup@sina.com>
2013-05-11 12:01:37 +0800

    add soap version validation when soap message is binded to object

---
+++ picoTest/picoTest/SOAPReaderTest.h
@@ -0,0 +1,13 @@
+//
+//  SOAPReaderTest.h
+//  PicoTest
+//
+//  Created by user on 13-5-11.
+//  Copyright (c) 2013年 LeanSoft Technology. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SOAPReaderTest : NSObject
+
+@end
